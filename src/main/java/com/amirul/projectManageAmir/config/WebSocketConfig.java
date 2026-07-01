6b135fc3d66c39e14c6c29b532531666219ea542
Amirul Momenin <amirulmomenin2k2@gmail.com>
2026-07-01 14:00:53 +0530

    trying to deploy

--- src/main/java/com/amirul/projectManageAmir/config/WebSocketConfig.java
+++ src/main/java/com/amirul/projectManageAmir/config/WebSocketConfig.java
@@ -2,14 +2,13 @@ package com.amirul.projectManageAmir.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebsocketConfig implements WebSocketMessageBrokerConfigurer{
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
 
 
